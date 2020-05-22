8039e4ecf152d2b7cff6a39f3960cd6387e9ad5a
Shreyas-Penkar <shreyaspenkar999@gmail.com>
2020-05-22 18:27:58 +0530

    api is now functional

--- back-end/app.py
+++ back-end/app.py
@@ -1,28 +1,62 @@
 from flask import Flask
 from flask import url_for,render_template,request
-from DataCleaner import Clean,Grammarize
+#from DataCleaner import Clean,Grammarize
+#from ModelDriver import generate
 
 app = Flask(__name__)
 
 PORT=8000
-data={                 # the API response
+TokenCount={  # Story Length (can be implemented later)
+    "Short":1023,
+    "Medium":1705,
+    "Long":2728
+}
+
+data={           # the API response
+    "Genre":"",
+    "LengthType":"short", # short/medium/long
+    "TokenCount":0,
+    "WordsCount":0,
     "InputStory":"",
     "OutputStory":""
 }
 
+def API(genre):  # API handler function
+    data["Genre"]=genre
+    text = request.args["inputText"]
+    text=(text.lstrip()).rstrip()
+    # data["InputStory"]=Grammarize(Clean(text))            # calling Data cleaning module
+    data["InputStory"]=text             # only for gpt-2 model 
+    # data["OutputStory"]=generate(data["InputStory"],genre)  # the text will be passed on to the model and the response will be sent back  
+    data["OutputStory"]=text*50  # just for demo 
+    data["WordsCount"]=len(data["OutputStory"].split())
+    data["TokenCount"]=len(data["OutputStory"])
+
+# Routes
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')  # we have to render kush ka site here
 
-@app.route('/api',methods=['GET'])   # the Model API endpoint
-def API():
-    text = request.args["input"]
-    text=(text.lstrip()).rstrip()
-    data["InputStory"]=Grammarize(Clean(text))  # calling Data cleaning module
-        # the text will be passed on to the model and the response will be sent back
-        
+@app.route('/api/adventure',methods=['GET'])   # the Model API adventure endpoint
+def adventure():
+   API("adventure")
+   return data
+
+    
+
+@app.route('/api/horror',methods=['GET'])   # the Model API horror endpoint
+def horror():
+    API("horror")
     return data
+
+
+
+@app.route('/api/action',methods=['GET'])   # the Model API action endpoint
+def action():
+    API("action")
+    return data
+
 
  
 @app.route('/command')   # the Command API endpoint
