3c4118f016f04b9eef574edbf63c7bef0d81818a
Kush Kothari <kushkothari2001@gmail.com>
2020-06-26 10:21:56 +0530

    Improved responsiveness of the UI

--- Web UI/Normal UI/script.js
+++ Web UI/Normal UI/script.js
@@ -8,27 +8,15 @@ function blueClick() {
 	root.style.setProperty("--main-opp-color", "#00fffc");
 }
 
-let main = document.getElementById("main");
-let side = document.getElementById("controls");
+// let main = document.getElementById("main");
+// let side = document.getElementById("controls");
 
-side.addEventListener("mouseenter", function () {
-	main.style.setProperty("left", "20vw");
-	console.log(main);
-});
-side.addEventListener("mouseleave", function () {
-	main.style.setProperty("left", "0vw");
-	console.log(main);
-});
-
-let txtinput = document.getElementById("text-input");
-
-txtinput.addEventListener("input", function () {
-	let text = txtinput.value;
-	if (text[text.length - 1] != "_") {
-		text = text + "_";
-	} else txtinput.value = text;
-});
-
+// side.addEventListener("mouseenter", function () {
+// 	side.style.setProperty("left", "19vw");
+// });
+// side.addEventListener("mouseleave", function () {
+// 	side.style.setProperty("left", "0vw");
+// });
 
 // Text to Speech
 const speakOnClick = (textInput) => {
@@ -37,4 +25,4 @@ const speakOnClick = (textInput) => {
 	const voiceName = window.speechSynthesis.getVoices()[0];
 	toSpeak.voice = voiceName;
 	tts.speak(toSpeak);
-}
+};
